3ea2110881d0b9ca833c66d7c4526fb16d8ba326
Danielle Gouveia <danielle.vieira9@etec.sp.gov.br>
2024-09-08 18:35:48 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("--- Minhas frutas preferidas ---\n");
+Console.WriteLine("1. Melancia");
+Console.WriteLine("2. Uva");
+Console.WriteLine("3. Banana");
+Console.WriteLine("4. Morango");
+Console.WriteLine("5. Maça");
